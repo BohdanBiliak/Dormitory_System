4c2e6383aa7ccc14db364934390e4cf26bab6272
Bohdan Biliak <arheroha@gmasil.com>
2025-07-03 18:16:30 +0100

    chore: add server project structure

---
+++ server/src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import * as cookieParser from 'cookie-parser'
+import {ConfigService} from "@nestjs/config";
+import {ValidationPipe} from "@nestjs/common";
+
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = app.get(ConfigService);
+  app.use(cookieParser(config.getOrThrow('COOKIE_SECRET')));
+  app.useGlobalPipes(new ValidationPipe({
+    transform:true
+  }));
+
+  app.enableCors({
+    origin: config.getOrThrow<string>('ALLOWED_ORIGIN'),
+    credentials: true,
+    exposeHeaders: ['set-cookie'],
+  })
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
